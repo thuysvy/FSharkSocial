553ff3931bf9f20a393defbd3ecc469f4530929c
thuysvy <150228593+thuysvy@users.noreply.github.com>
2024-09-14 10:17:26 +0700

    NEW

---
+++ BEFShark/FShark_BE/src/main/java/com/poly/fshark_be/FSharkBeApplication.java
@@ -0,0 +1,13 @@
+package com.poly.fshark_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FSharkBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FSharkBeApplication.class, args);
+    }
+
+}
