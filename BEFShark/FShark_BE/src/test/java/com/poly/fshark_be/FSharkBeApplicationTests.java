a849f120461c49134c5df47b849cc81337fb3e83
thuann17 <117891819+thuann17@users.noreply.github.com>
2024-09-17 13:04:39 +0700

    1

---
+++ BEFShark/FShark_BE/src/test/java/com/poly/fshark_be/FSharkBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poly.fshark_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FSharkBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
